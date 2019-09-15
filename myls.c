4c6dee6a06416ea72a096cd8981876705e196399
Sammy <svancleve@middlebury.edu>
2019-09-15 14:36:03 -0400

    add skeleton myls file and blank makefile

---
+++ myls.c
@@ -0,0 +1,18 @@
+int main(int argc, char *argv[]){
+
+	/* check arguments */
+
+	/* if no arguments, list files in curent directory */
+		/* open current directory */
+		/* print out names of files */
+
+	/* otherwise, parse arguments */
+
+	/* given -a, include hidden files */
+
+	/* given -l, include extra info */
+
+	/* given directory name, list items in directory */
+
+	/* given file name, output file name */
+}
