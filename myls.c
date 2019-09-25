18fd1942d590ce9660a81017bd9d036010c73f34
Sammy <svancleve@middlebury.edu>
2019-09-24 20:49:15 -0400

    add starting functionality for -l flag, README files

--- myls.c
+++ myls.c
@@ -2,12 +2,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <pwd.h>
+#include <grp.h>
+#include <time.h>
+#include <locale.h>
 
 int main(int argc, char *argv[]){
     DIR *directory;
     struct dirent *entry;
     int aflag = 0;
     int lflag = 0;
+    struct stat statbuf;
+    struct passwd *pwd;
+    struct group *grp;
+    int status;
 
     /* check arguments */
     int option;
@@ -33,17 +43,29 @@ int main(int argc, char *argv[]){
     char *filename;
     while ((entry = readdir(directory))!=NULL){
         filename=entry->d_name;
-        if (filename[0]=='.'){
-            if (aflag==1){
+        if (lflag == 0){
+            if (filename[0]=='.'){
+                if (aflag==1){
+                    printf("%s\n", filename);
+                }
+            }
+            else{
                 printf("%s\n", filename);
             }
         }
         else{
+            stat(entry->d_name, &statbuf);
+            //printf("%10.10s", statbuf.st_mode&0777);
+            printf("%4d", statbuf.st_nlink);
+            if ((pwd = getpwuid(statbuf.st_uid)) != NULL){
+                printf(" %-8.8s", pwd->pw_name);
+            }
+            if ((grp = getgrgid(statbuf.st_gid)) != NULL){
+                printf(" %-8.8s", grp->gr_name);
+            }
             printf("%s\n", filename);
         }
     }
-
-    /* given -l, include extra info */
 
     /* given directory name, list items in directory */
 
